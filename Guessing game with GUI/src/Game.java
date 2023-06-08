e4f500f999bfd7b71aa14449c5b1890ad6b1d877
salma-mohamed-37 <lulymohamed37@gmail.com>
2023-06-08 22:06:02 +0300

    improve gui

--- Guessing game with GUI/src/Game.java
+++ Guessing game with GUI/src/Game.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class Game extends JFrame
 {
+	JPanel mainPanel;
+	JPanel titlePanel ;
+	JPanel playPanel ;
+	JPanel messagePanel;
 	JLabel title = new JLabel() ;
 	JLabel lower = new JLabel();
 	JLabel and = new JLabel();
@@ -29,25 +33,65 @@ public class Game extends JFrame
 
 	Game()
 	{			
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		titlePanel = new JPanel (new FlowLayout());
+		playPanel = new JPanel();
+		messagePanel = new JPanel();
+
 		title.setText("You have to guess a number between");
 		lower.setText("1");
 		and.setText("&");
 		upper.setText("100");
 		message.setText("Enter your guess");
 		
-		add(title);
-		add(lower);
-		add(and);
-		add(upper);
-		add(message);
-		add(answer);
-		add(guess);
-		add(restart);
+		titlePanel.add(title);
+		titlePanel.add(lower);
+		titlePanel.add(and);
+		titlePanel.add(upper);
+		
+		playPanel.add(answer);
+		playPanel.add(guess);
+		playPanel.add(restart);
+		
+		messagePanel.add(message);
+		
+		titlePanel.setMaximumSize(new Dimension(700, 100));
+		messagePanel.setMaximumSize(new Dimension(700, 100));
+		
+		Font myFont = new Font("Arial", Font.BOLD, 30);
+		
+		title.setFont(myFont);
+		lower.setFont(myFont);
+		and.setFont(myFont);
+		upper.setFont(myFont);
+		message.setFont(myFont);
+		answer.setFont(myFont);
+		guess.setFont(myFont);
+		restart.setFont(myFont);
 		
 		guess.addActionListener(new ButtonClick());
 		restart.addActionListener(new ButtonClick());
-		setLayout(new FlowLayout());
-		setSize(355, 350);
+
+		mainPanel.add(Box.createVerticalGlue()); 
+		mainPanel.add(titlePanel);
+		mainPanel.add(messagePanel);
+		mainPanel.add(playPanel);
+		mainPanel.add(Box.createVerticalGlue());
+	
+		getContentPane().add(mainPanel).setBackground(Color.lightGray);
+		
+		mainPanel.setBackground(Color.PINK);
+		titlePanel.setBackground(Color.PINK);
+		messagePanel.setBackground(Color.PINK);
+		playPanel.setBackground(Color.PINK);
+		
+		guess.setBackground(Color.BLACK);
+		guess.setForeground(Color.PINK);
+		
+		
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		restart.setVisible(false);
 		g.begin();
